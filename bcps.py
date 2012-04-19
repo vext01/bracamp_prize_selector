e0125de5c2c13d82d5c94b47887467648a2b5e5d
vext01 <vext01@gmail.com>
2012-04-19 11:44:36 +0100

    jumble effect

--- bcps.py
+++ bcps.py
@@ -15,12 +15,16 @@ class uplink_text:
         self.rng = Random()
         self.width = width
         self.text = text.center(width)
-        self.mask = self.make_mask()
+        self.widget = urwid.Text("", align='center')
 
     def update(self):
-        self.widget = urwid.Text(self.mask, align='center')
+        mask = self.make_mask()
+        self.widget.set_text(mask)
 
 
+# has to be a function not a method
+def alarm_handler(loop, selector):
+    selector.update()
 
 class barcamp_prize_selector:
 
@@ -31,6 +35,12 @@ class barcamp_prize_selector:
         for p in peep_list:
             self.people.append(uplink_text(p, 40))
             i = i + 1
+        self.loop = None
+
+    def update(self):
+        for p in self.people:
+            p.update()
+        self.loop.set_alarm_in(.1, alarm_handler, self)
 
     def render(self):
 
@@ -42,9 +52,11 @@ class barcamp_prize_selector:
             
         pile = urwid.Pile(widgets)
         fill = urwid.Filler(pile)
-        loop = urwid.MainLoop(fill)
-        loop.run()
+        self.loop = urwid.MainLoop(fill)
+        self.update()
+        self.loop.run()
 
 if __name__ == "__main__":
-    b = barcamp_prize_selector(["edd123", "jim234"])
+    b = barcamp_prize_selector(["edd123", "jim234", "bob-jibble",
+        "lalalalalala", "ohnoesohnoesohnoes"])
     b.render()
